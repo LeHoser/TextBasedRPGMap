8b989143612fc87d226bb34e245a9c499326cb22
Nathan Lloyd <nathan.lloyd.7003@gmail.com>
2022-11-10 13:08:06 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextBasedRPGMap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Text Based RPG Map");
+            Console.WriteLine();
+
+            Console.ReadKey(true);
+        }
+    }
+}
